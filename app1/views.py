a7f88ac5cd0e702fa456d82ed2707c164d1f54ec
vasky215 <mmanterolalopez@gmail.com>
2026-09-17 23:40:26 -0300

    Agrega templates con navegación y styles, redirect en la raíz

--- app1/views.py
+++ app1/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
 
 def inicio(request):
-    return HttpResponse("<h1>Bienvenido a App1</h1><p>Esta es la vista de inicio.</p>")
+    return render(request, 'app1/inicio.html')
 
 def acerca(request):
-    return HttpResponse("<h1>Acerca de App1</h1><p>Esta app fue creada para la Evaluación Sumativa 1.</p>")
+    return render(request, 'app1/acerca.html')
